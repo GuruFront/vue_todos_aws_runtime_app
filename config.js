441516f81aa6aca807f1497b027a08ebb028edc7
Kofanov Ruslan <ruslan.gunder@gmail.com>
2023-05-13 16:06:12 +0300

    new key;

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ export const config = {
     "aws_appsync_graphqlEndpoint": "https://6fh2we4l5fbilkhgoa5n3uonqu.appsync-api.eu-central-1.amazonaws.com/graphql",
     "aws_appsync_region": "eu-central-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-sa3mpes6b5by7oeictgj56rwau",
+    "aws_appsync_apiKey": "da2-kg74kibfw5dv3n4uvf7lg6qkpi",
 }
